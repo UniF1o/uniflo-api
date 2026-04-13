3b8cbbdd670f8fa49bd5e0b189f936103ce95bb7
AndzisoKhosa <138158612+AndzisoKhosa@users.noreply.github.com>
2026-04-13 20:13:45 +0200

    feat: add phase1 SQLModel models and Alembic migration (#9)
    * feat: add phase1 SQLModel models and Alembic migration

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 from app.config import settings
 from alembic import context
 
+from app.models import StudentProfile, AcademicRecord, Document
+from sqlmodel import SQLModel
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -16,9 +19,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ app/models/student_profile.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timezone, date
+from typing import Optional
+from sqlmodel import SQLModel, Field 
+import uuid
+from sqlalchemy import Column, DateTime
+
+class StudentProfile(SQLModel, table=True):
+    __tablename__ = "student_profiles"
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: uuid.UUID = Field(foreign_key="users.id", nullable=False, unique=True, index=True)
+    first_name: str = Field(nullable=False)
+    last_name: str = Field(nullable=False)
+    id_number: str = Field(unique=True)
+    date_of_birth: date
+    phone: str = Field(nullable=False)
+    address: str = Field(nullable=False)
+    nationality: str = Field(nullable=False)
+    gender: str = Field(nullable=False)
+    home_language: str = Field(nullable=False)
+    updated_at: Optional[datetime] = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column= Column(
+            DateTime(timezone=True),
+            onupdate=lambda:  datetime.now(timezone.utc),
+            nullable=True
+        )
+        )
+
+    
